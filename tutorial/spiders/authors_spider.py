868c32e9ab066fc3058de166dea3f7c05a308edb
Harry Wang <harryjwang@gmail.com>
2019-05-30 16:42:35 -0400

    added author spider

---
+++ tutorial/spiders/authors_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        # follow links to author pages
+        for href in response.css('.author + a::attr(href)'):
+            yield response.follow(href, self.parse_author)
+
+        # follow pagination links
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
